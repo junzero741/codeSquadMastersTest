6835981be2b837e55fc7fda0f7a1f3a1cf41f93c
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-12-09 20:33:42 +0900

    Add : DOM 선택 변수 생성
    * 캔버스 선택 상수 추가
    * 입력칸 상수 추가
    * 버튼 상수 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-//init
+const canvas = document.querySelector(".rectangle"),
+    input = document.getElementById("input"),
+    button = document.getElementById("button");
+let cube =  [["R","R","W"], 
+            ["G", "C", "W"],
+            ["G", "B", "B"]];
+let inputArray = [];
