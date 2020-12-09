4b5aabb653d49b42699856093de5efa348b6e621
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-12-09 20:51:35 +0900

    Add : 큐브 회전 함수 추가
    * inputArray 를 돌면서 원소 문자열 검사
    * 문자열에 따라 각기 다른 큐브 회전 로직 구현

--- src/index.js
+++ src/index.js
@@ -21,6 +21,63 @@ function mergeSingleQuote() {
 } 
 
 
+function checkInput() {
+    for(let i=0; i < inputArray.length; i++) {
+        moveCube(i)
+     }
+}
+
+function moveCube(i) {
+    if(inputArray[i] === "U") {
+        cube[0].push(cube[0].shift());
+        renderCube("U");
+    }
+    if(inputArray[i] === "U`") {
+        cube[0].unshift(cube[0].pop());
+        renderCube("U`");
+    }
+    if(inputArray[i] === "B") {
+        cube[2].unshift(cube[2].pop());
+        renderCube("B`");
+    }
+    if(inputArray[i] === "B`") {
+        cube[2].push(cube[2].shift());
+        renderCube("B");
+    }
+    if(inputArray[i] === "R") {
+        const temp = cube[0].pop();
+        cube[0].push(cube[1].pop());
+        cube[1].push(cube[2].pop());
+        cube[2].push(temp);
+        renderCube("R");
+    }
+    if(inputArray[i] === "R`") {
+        const temp = cube[2].pop();
+        cube[2].push(cube[1].pop());
+        cube[1].push(cube[0].pop());
+        cube[0].push(temp);
+        renderCube("R`");
+    }
+    if(inputArray[i] === "L") {
+        const temp = cube[2].shift();
+        cube[2].unshift(cube[1].shift());
+        cube[1].unshift(cube[0].shift());
+        cube[0].unshift(temp);
+        renderCube("L");
+    }
+    if(inputArray[i] === "L`") {
+        const temp = cube[0].shift();
+        cube[0].unshift(cube[1].shift());
+        cube[1].unshift(cube[2].shift());
+        cube[2].unshift(temp);
+        renderCube("L`");
+    }
+    if(inputArray[i] === "Q") {
+        document.write("BYE~");
+    }
+}
+
+
 function handleEvent() {
     button.addEventListener("click", mergeSingleQuote);
 }
@@ -28,6 +85,7 @@ function handleEvent() {
 function init() {
     canvas.innerHTML =  `${cube[0]} <br> ${cube[1]} <br> ${cube[2]}`; 
     handleEvent();
+    checkInput();
 }
 
 init();
