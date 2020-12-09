e5405d88e20dea24312b7761b0ce28ab66e0c296
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-12-09 17:31:56 +0900

    Add : 입력받은 문자열을 textArr에 저장했다.
    * 문자열을 총 3부분으로 나누어 각각 변수에 저장했다.
    * 영단어를 뒤집기 위해 한 글자씩 쪼개어 저장했다.
    * 숫자는 prompt 입력당시 문자열로 입력되어서, 숫자화했다.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// init
-
 let wordArr = [],
     direction,
     num;
@@ -7,3 +5,25 @@ let wordArr = [],
 const newWordArr = [],
     text = prompt("영단어 숫자 방향 순으로 입력하세요. ( Ex: crong 2 L )");
 
+function setInput() {
+     // 영단어 배열화
+     let textArr = text.split(" ");
+     wordArr = textArr[0].split("");
+ 
+     // 방향 지정
+     direction = textArr[2];
+ 
+     // 숫자 검사
+     textArr[1] = Number(textArr[1]);
+     num = textArr[1];
+     if (num < 0) {
+         num = Math.abs(num);
+         direction = ChangeDirection(direction);
+     }
+}
+
+function init() {
+    setInput();
+}
+
+init();
