53f821bf918228310c51c374f5d92d115b7c8066
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-12-09 20:42:16 +0900

    Add : 버튼 클릭하면 입력한 텍스트를 배열에 저장
    * 인용구 " ` " 를 앞 원소에 합치는 기능 구현
    * 이벤트 관리 함수 handleEvent 추가
    * init 함수 추가
    * 초기 큐브 렌더링 추가

--- src/index.js
+++ src/index.js
@@ -5,3 +5,29 @@ let cube =  [["R","R","W"],
             ["G", "C", "W"],
             ["G", "B", "B"]];
 let inputArray = [];
+
+
+
+function mergeSingleQuote() {
+    const inputText = input.value;
+    inputArray = inputText.split("");
+    for(let i = 0; i < inputArray.length; i++) {
+        if(inputArray[i] === "`") {
+            inputArray[i-1] = inputArray[i-1] + "`";
+            inputArray.splice(i,1);
+        }
+    }
+    console.log(inputArray);
+} 
+
+
+function handleEvent() {
+    button.addEventListener("click", mergeSingleQuote);
+}
+
+function init() {
+    canvas.innerHTML =  `${cube[0]} <br> ${cube[1]} <br> ${cube[2]}`; 
+    handleEvent();
+}
+
+init();
