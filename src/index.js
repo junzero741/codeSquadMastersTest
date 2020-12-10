6dbc44905f6053a0ce6bdfa5984d7af081870553
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-12-10 14:21:50 +0900

    Add : DOM 객체 생성, 큐브 자료구조 생성
    * 큐브의 각 면을 DOM 객체로 생성했다.
    * 입력칸과 버튼을 DOM 객체로 생성했다.
    * 큐브의 각 면의 자료구조를 2차원 배열로 생성했다.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// init
+const canvasTop = document.querySelector(".canvasTop"),
+    canvasLeft = document.querySelector(".canvasLeft"),
+    canvasFront = document.querySelector(".canvasFront"),
+    canvasRight = document.querySelector(".canvasRight"),
+    canvasBack = document.querySelector(".canvasBack"),
+    canvasBottom = document.querySelector(".canvasBottom"),
+    input = document.getElementById("input"),
+    button = document.getElementById("button");
+
+let cubeTop =   [["B","B","B"], 
+                 ["B","B","B"],
+                 ["B","B","B"]];
+
+let cubeLeft =  [["W","W","W"], 
+                ["W","W","W"],
+                ["W","W","W"]];
+            
+let cubeFront = [["O","O","O"], 
+                ["O","O","O"],
+                ["O","O","O"]];
+                
+let cubeRight = [["G","G","G"], 
+                ["G","G","G"],
+                ["G","G","G"]];
+
+let cubeBack = [["Y","Y","Y"], 
+                ["Y","Y","Y"],
+                ["Y","Y","Y"]];
+
+let cubeBottom =[["R","R","R"], 
+                ["R","R","R"],
+                ["R","R","R"]];
